ccc85bf68e335766d03bc8dffd274921e0ab2df7
yumyummmmmy <rol.lol.lor@gmail.com>
2024-04-02 15:49:32 +0900

    メイン完成

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 import "./FoodItem.css";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  //フードアイテムカウンター
+
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount ? (
+
+        {!cartItems[id] ? (
           <img
-            class="add"
-            onClick={() => setItemCount((prev) => prev + 1)}
+            className="add"
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt=""
           />
         ) : (
-          <div className="food-item-counter"></div>
+          //カート追加or減らす
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
         )}
       </div>
       <div className="food-item-info">
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,36 @@
-import { createContext } from "react";
+import { useEffect } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
-  // valueプロパティを通じてコンテキストデータ（foodlist)を渡す
+  const [cartItems, setCartItems] = useState({});
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      // もしitemIdがcartItemsにあるならsetCartItemsが呼び出される
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      //すでにカートにあるアイテムに+1をする
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+  ///すでにカードにあるアイテムに-1をする
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  // valueプロパティを通じて子に渡す
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
   return (
     <StoreContext.Provider value={contextValue}>
